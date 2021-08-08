5999f05c027a02dad00c67268278125024e6a8c1
prashant <csprshantphuse@gmail.com>
2021-08-08 22:28:57 +0530

    Project created

---
+++ app/src/main/java/com/example/dooleapplication/DooleView.java
@@ -0,0 +1,77 @@
+package com.example.dooleapplication;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.recyclerview.widget.ItemTouchHelper;
+
+public class DooleView extends View  {
+    Paint paint;
+    Path path;
+    public DooleView(Context context) {
+        super(context);
+        init(null);
+    }
+
+    public DooleView(Context context, @Nullable @org.jetbrains.annotations.Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs);
+    }
+
+    public DooleView(Context context, @Nullable @org.jetbrains.annotations.Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(attrs);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public DooleView(Context context, @Nullable @org.jetbrains.annotations.Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(attrs);
+    }
+    public void init(AttributeSet attr){
+        paint=new Paint();
+        paint.setColor(Color.GREEN);
+        paint.setAntiAlias(true);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeJoin(Paint.Join.ROUND);
+        paint.setStrokeWidth(8f);
+        path=new Path();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawColor(Color.WHITE);
+        canvas.drawPath(path,paint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x=event.getX();
+        float y= event.getY();
+        switch(event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                path.moveTo(x,y);
+                return true;
+            case MotionEvent.ACTION_UP:
+                break;
+            case MotionEvent.ACTION_MOVE:
+                path.lineTo(x,y);
+                break;
+            default:
+                return false;
+
+        }
+        postInvalidate();
+        return false;
+    }
+}
